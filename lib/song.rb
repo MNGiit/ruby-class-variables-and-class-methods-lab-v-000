5921bb8d0c5b46a0cf5c91387786bf64d653f4e2
Mohammed Noori <kmanoorib@gmail.com>
2018-06-02 22:18:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,9 @@ class Song
   
   def self.genre_count
     genre_hash = {}
-    
+    @@genres.each do |genre|
+      genre_hash[genre]? genre_hash[genre] +=1 : genre_hash[genre] = 1
+    end
+    genre_hash
   end
 end
