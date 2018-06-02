80b15052c9b82b45736b762a987c615457877941
Mohammed Noori <kmanoorib@gmail.com>
2018-06-02 21:48:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count+=1
-    @@artists.push(artist)
-    @@genres.push(genre)
+    @@artists.push(artist) if !@@artists.include?(artist)
+    @@genres.push(genre) if !@@genres.include?(genre)
   end
 
   def self.count
