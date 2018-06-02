e9d59453fd60c4f3b05e4a507dcaf86b3e262a73
Mohammed Noori <kmanoorib@gmail.com>
2018-06-02 21:47:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@artists = []
   @@genres = []
   
   def initialize (name, artist, genre)
@@ -8,6 +9,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count+=1
+    @@artists.push(artist)
     @@genres.push(genre)
   end
 
